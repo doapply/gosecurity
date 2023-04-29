f9d0863a88cd988ec98a613d2c6e80e44258ddea
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-04-28 18:56:25 -0700

    Create doc.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>

---
+++ v2/internal/installer/doc.go
@@ -0,0 +1,6 @@
+package installer
+
+// package install provides helper functions for all download and installation of following tasks:
+// 1. downloading and install gosecurity templates
+// 2. download and update gosecurity binary (i.e self update)
+// 3. version check for gosecurity binary & templates
